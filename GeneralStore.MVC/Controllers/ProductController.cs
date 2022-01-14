d27839599d078ae022e767bcbf66a88556f9c1f4
Anna Heselbarth <annaheselbarth@gmail.com>
2022-01-13 19:22:34 -0500

    Add project files.

---
+++ GeneralStore.MVC/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using GeneralStore.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStore.MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        //Add the application DB context (link to the database)
+        private ApplicationDbContext _db = new ApplicationDbContext();
+        // GET: Product
+        public ActionResult Index()
+        {
+            // See below (modifying ApplicationDbContext class)
+            return View(_db.Products.ToList());
+        }
+
+        // GET: Product
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
