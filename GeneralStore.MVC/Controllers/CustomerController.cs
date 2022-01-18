3e263c7cc5658f3ecb0e91c2c660b9a30160c715
Anna Heselbarth <annaheselbarth@gmail.com>
2022-01-18 18:55:37 -0500

    Details added

--- GeneralStore.MVC/Controllers/CustomerController.cs
+++ GeneralStore.MVC/Controllers/CustomerController.cs
@@ -95,5 +95,23 @@ namespace GeneralStore.MVC.Controllers
             }
             return View(customer);
         }
+
+        //GET: Details
+        //Product/Details/{id}
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = _db.Customers.Find(id);
+
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(customer);
+        }
     }
 }
