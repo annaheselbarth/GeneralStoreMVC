f4dc20bc509e2e6f04a2fcefa5e3a7a99c1ef7ab
Anna Heselbarth <annaheselbarth@gmail.com>
2022-01-18 19:12:49 -0500

    Transaction model added

---
+++ GeneralStore.MVC/Models/Transaction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStore.MVC.Models
+{
+    public class Transaction
+    {   
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        [Display(Name = "Product Name")]
+        public string Name { get; set; }
+
+        [Required]
+        public int Amount { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int TransactionId { get; set; }
+        public DateTime DateOfTransaction { get; set; }
+    }
+}
